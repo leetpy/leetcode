3f34e89a690ab5ea7835a75f3f30c17ebaf2726d
leesea <leetpy2@gmail.com>
2018-08-22 19:38:23 +0800

    Update isPalindrome.go

--- algorithms/go/isPalindrome/isPalindrome.go
+++ algorithms/go/isPalindrome/isPalindrome.go
@@ -25,6 +25,24 @@ func isPalindrome(x int) bool {
 	return true
 }
 
+// 翻转方式
+func isPalindrome2(x int) bool {
+    if x < 0 || (x % 10 == 0 && x != 0) { 
+        return false
+    }   
+
+    // 已经翻转的数字
+    revertedNumber := 0
+    for {
+        if x <= revertedNumber {  // 控制只翻转一半
+            break
+        }
+        revertedNumber = revertedNumber*10 + x%10
+        x /= 10
+    }                                                                                                                                                                         
+    return x == revertedNumber || x == revertedNumber/10 // 考虑奇偶
+}
+
 func main() {
 	fmt.Println(isPalindrome(121))
 }
